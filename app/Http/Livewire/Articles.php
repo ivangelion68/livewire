749c3f19e67f57cad764b7973615b297b221f92e
Ivan Altamirano(Nipigon) <ivan.altamirano@outlook.com>
2023-02-05 10:48:29 -0300

    Propiedades en Livewire

--- app/Http/Livewire/Articles.php
+++ app/Http/Livewire/Articles.php
@@ -3,9 +3,15 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-
+use App\Models\Article;
 class Articles extends Component
 {
+    public $articles;
+
+    public function mount(){
+        $this->articles=Article::all();
+    }
+
     public function render()
     {
         return view('livewire.articles');
--- resources/views/livewire/articles.blade.php
+++ resources/views/livewire/articles.blade.php
@@ -1,1 +1,8 @@
-<h1>Article List</h1>
+<div>
+    <h1>Article List</h1>
+    <ul>
+        @foreach($articles as $article)
+        <li>{{$article->title}}</li>
+        @endforeach
+    </ul>
+</div>
